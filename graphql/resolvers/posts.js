71dfbbb1d5b9adcb06c96e77a6fca00c781600e2
androcpp <georgeandronache.cpp@gmail.com>
2022-04-10 00:12:52 +0300

    server done

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
-const {AuthenticationError} = require('apollo-server');
+const {AuthenticationError, UserInputError} = require('apollo-server');
+const { argsToArgsConfig } = require('graphql/type/definition');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../utils/check-auth');
@@ -35,6 +36,10 @@ module.exports = {
             const user = checkAuth(context);
             console.log(user);
 
+            if(args.body.trim() === '') {
+                throw new Error('Post body must not be empty');
+            }
+
             const newPost = new Post({
                 body,
                 user: user.id,
@@ -61,6 +66,29 @@ module.exports = {
             }
             catch(err) {
                 throw new Error(err);
+            }
+        },
+        async likePost(_, {postId}, context) {
+            const {username} = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            if(post) {
+                if(post.likes.find(like => like.username === username)) {
+                    // Post already liked, unlike it.
+                    post.likes = post.likes.filter(like => like.username !== username);
+                }
+                else {
+                    // Not liked, like post
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    });
+                }
+                await post.save();
+                return post;
+            }
+            else {
+                throw new UserInputError('Post not found');
             }
         }
     }
