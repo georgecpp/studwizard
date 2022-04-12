56146e63a14aa2176a97b460efad33046593b5b8
androcpp <georgeandronache.cpp@gmail.com>
2022-04-13 00:24:22 +0300

    login/register done - starting authcontext

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,95 @@
-import React from "react";
+import gql from "graphql-tag";
+import React, { useState } from "react";
+import {Button, Form} from "semantic-ui-react";
+import {useMutation} from '@apollo/client';
+import {useHistory} from "react-router-dom";
 
 function Login() {
+
+    const history = useHistory();
+    const [errors, setErrors] = useState({});
+    const [values, setValues] = useState({
+        username: '',
+        password: '',
+    });
+
+    const onChange = (event) => {
+        setValues({...values,[event.target.name]: event.target.value});
+    }
+    
+    const [loginUser, {loading}] = useMutation(LOGIN_USER , {
+        update(_, result) {
+            console.log(result);
+            history.push('/')
+        },
+        onError(err) {
+            setErrors(err.graphQLErrors[0].extensions.errors);
+        },
+        variables: values
+    });
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        loginUser();
+    }
+
+
+
     return (
-        <div><h1>Login Page</h1></div>
-    )
+        <div className="form-container">
+            <Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ''}>
+                <h1>Login</h1>
+                <Form.Input 
+                    label="Username"
+                    placeholder="Username.."
+                    name="username"
+                    type="text"
+                    value={values.username}
+                    error={errors.username ? true : false}
+                    onChange={onChange}
+                />
+                 <Form.Input 
+                    label="Password"
+                    placeholder="Password.."
+                    name="password"
+                    type="password"
+                    value={values.password}
+                    error={errors.password ? true : false}
+                    onChange={onChange}
+                />
+                <Button type="submit" primary>
+                    Login
+                </Button>
+            </Form>
+            {Object.keys(errors).length > 0 && (
+                <div className="ui error message">
+                <ul className="list">
+                    {Object.values(errors).map((value) => (
+                    <li key={value}>{value}</li>
+                    ))}
+                </ul>
+                </div>
+            )}
+        </div>
+    );
 }
+
+const LOGIN_USER = gql`
+  mutation login(
+    $username: String!
+    $password: String!
+  ) {
+    login(
+        username: $username
+        password: $password
+    ) {
+      id
+      email
+      username
+      createdAt
+      token
+    }
+  }
+`;
 
 export default Login;
